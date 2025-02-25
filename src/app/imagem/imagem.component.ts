d48f485448149d2a217dccd2a57002164a25d4c9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-02-25 14:13:24 -0300

    chore: base imagem component

---
+++ src/app/imagem/imagem.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-imagem',
+  imports: [],
+  templateUrl: './imagem.component.html',
+  styleUrl: './imagem.component.scss'
+})
+export class ImagemComponent {
+
+}
