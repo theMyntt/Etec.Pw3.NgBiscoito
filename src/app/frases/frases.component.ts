54e563ea519169dfb2a9153b5c5b8fe4cb331aec
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com.br>
2025-02-25 14:18:15 -0300

    chore: base phrase component

---
+++ src/app/frases/frases.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-frases',
+  imports: [],
+  templateUrl: './frases.component.html',
+  styleUrl: './frases.component.scss'
+})
+export class FrasesComponent {
+
+}
